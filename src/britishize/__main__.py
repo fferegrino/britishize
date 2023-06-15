ae01040357cf8352b281ea24ca46bb4dff24f4a1
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-06-15 10:19:02 +0100

    Run `black .` in all the codebase

--- src/britishize/__main__.py
+++ src/britishize/__main__.py
@@ -3,13 +3,16 @@ from britishize.britishize import britishize
 
 app = typer.Typer()
 
+
 @app.command()
-def to_british(text:str):
+def to_british(text: str):
     britishized = britishize(text)
     print(britishized)
 
+
 def main():
     app()
+
 
 if __name__ == "__main__":
     main()
