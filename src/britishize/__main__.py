60a2f5c4cd5bff445ed0edd977b5897ad77e9fd7
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-06-15 10:15:06 +0100

    Add code for the script
    Now you can call it from within the environment:
    
    ```shell
    poetry install
    poetry shell
    python -m britishize "cookie"
    ```

---
+++ src/britishize/__main__.py
@@ -0,0 +1,15 @@
+import typer
+from britishize.britishize import britishize
+
+app = typer.Typer()
+
+@app.command()
+def to_british(text:str):
+    britishized = britishize(text)
+    print(britishized)
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    main()
